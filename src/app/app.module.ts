b1413350be5b494fc132e5955b2356aeff56fdcf
Your Name <jairog12@outlook.com>
2017-08-14 14:01:21 -0500

    Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,19 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAZQnvrNjFgz02JFCmgYznkSB2iYbonuwU",
+  authDomain: "gestur-app.firebaseapp.com",
+  databaseURL: "https://gestur-app.firebaseio.com",
+  projectId: "gestur-app",
+  storageBucket: "gestur-app.appspot.com",
+  messagingSenderId: "672997972871"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +27,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireAuth } from "angularfire2/auth";
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,29 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  displayName;  
 
+  constructor(public navCtrl: NavController,
+    private afAuth: AngularFireAuth) {
+      afAuth.authState.subscribe(user => {
+        if (!user) {
+          this.displayName = null;        
+          return;
+        }
+        this.displayName = user.displayName;      
+      });
+  }
+
+  signInWithGoogle() {
+    this.afAuth.auth
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(res => {
+        this.navCtrl.setRoot('ListadoPage')
+      });
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut();
   }
 
 }
